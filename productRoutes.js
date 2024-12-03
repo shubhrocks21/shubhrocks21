eaea54503c53ae771802b8f85482af01acc58a04
shubhrocks21 <95875817+shubhrocks21@users.noreply.github.com>
2024-12-03 14:53:52 +0530

    Update productRoutes.js

--- productRoutes.js
+++ productRoutes.js
@@ -6,11 +6,3 @@ router.get('/', productController.getAllProducts);
 router.post('/seed', productController.seedProducts);
 
 module.exports = router;
-const express = require('express');
-const router = express.Router();
-const productController = require('../controllers/productController');
-
-router.get('/', productController.getAllProducts);
-router.post('/seed', productController.seedProducts);
-
-module.exports = router;
