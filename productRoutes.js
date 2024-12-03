6f7fee838a40d830efe4a5ee7cc2336695505717
shubhrocks21 <95875817+shubhrocks21@users.noreply.github.com>
2024-12-03 13:10:56 +0530

    Create productRoutes.js

---
+++ productRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/productController');
+
+router.get('/', productController.getAllProducts);
+router.post('/seed', productController.seedProducts);
+
+module.exports = router;
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/productController');
+
+router.get('/', productController.getAllProducts);
+router.post('/seed', productController.seedProducts);
+
+module.exports = router;
